abc9bb3a7f5a2279fb610a19cfd70247a13e0980
Liujin <1287784468@qq.com>
2021-05-25 10:21:37 +0800

    完善“排课管理”的新增

---
+++ src/pages/Manager/BasicalSettings/ClassManagement/components/AddClass.tsx
@@ -0,0 +1,105 @@
+/* eslint-disable no-console */
+import ProFormFields from '@/components/ProFormFields';
+import { Button, Drawer } from 'antd';
+import type { FC } from 'react';
+import { useState } from 'react';
+
+type AddClassProps = {
+  visible: boolean;
+  onClose: () => void;
+  readonly?: boolean;
+  formValues?: Record<string, any>;
+};
+const formLayout = {
+  labelCol: {},
+  wrapperCol: {},
+};
+
+const AddClass: FC<AddClassProps> = ({ visible, onClose, readonly, formValues }) => {
+  const [form, setForm] = useState<any>();
+
+  const onFinish = (values: any) => {
+    console.log('onFinish131', values);
+  };
+
+  const handleSubmit = () => {
+    form.submit();
+  };
+
+  const formItems: any[] = [
+    {
+      type: 'input',
+      readonly,
+      label: '班级名称：',
+      name: 'BJMC',
+      key: 'BJMC',
+    },
+    {
+      type: 'select',
+      readonly,
+      label: '上课地点：',
+      name: 'SKDD',
+      key: 'SKDD',
+    },
+    {
+      type: 'select',
+      readonly,
+      label: '授课老师：',
+      name: 'SKLS',
+      key: 'SKLS',
+    },
+    {
+      type: 'select',
+      readonly,
+      label: '助教老师：(可多选)',
+      name: 'ZJLS',
+      key: 'ZJLS',
+      fieldProps: {
+        mode: 'multiple',
+      },
+    },
+    {
+      type: 'textArea',
+      readonly,
+      label: '简介：',
+      name: 'JJ',
+      key: 'JJ',
+    },
+  ];
+  return (
+    <div>
+      <Drawer
+        title="新增班级"
+        width={480}
+        onClose={onClose}
+        visible={visible}
+        bodyStyle={{ paddingBottom: 80 }}
+        footer={
+          <div
+            style={{
+              textAlign: 'right',
+            }}
+          >
+            <Button onClick={onClose} style={{ marginRight: 16 }}>
+              取消
+            </Button>
+            <Button onClick={handleSubmit} type="primary">
+              保存
+            </Button>
+          </div>
+        }
+      >
+        <ProFormFields
+          layout="vertical"
+          onFinish={onFinish}
+          setForm={setForm}
+          formItems={formItems}
+          formItemLayout={formLayout}
+          values={formValues}
+        />
+      </Drawer>
+    </div>
+  );
+};
+
+export default AddClass;
--- src/pages/Manager/BasicalSettings/ClassManagement/index.tsx
+++ src/pages/Manager/BasicalSettings/ClassManagement/index.tsx
@@ -1,202 +1,162 @@
 /* eslint-disable no-console */
-import PageContainer from "@/components/PageContainer";
-import type { ProColumns } from "@ant-design/pro-table";
-import ProTable from "@ant-design/pro-table";
-import { Divider} from "antd";
+import PageContainer from '@/components/PageContainer';
+import type { ProColumns } from '@ant-design/pro-table';
+import ProTable from '@ant-design/pro-table';
+import { Divider } from 'antd';
 import styles from './index.less';
-import type { RoomItem } from "./data";
-import { Button } from "antd";
-import { theme } from "@/theme-default";
-import type { FormInstance } from 'antd';
-import { listData } from "./mock";
+import type { RoomItem } from './data';
+import { Button } from 'antd';
+import { theme } from '@/theme-default';
+import { listData } from './mock';
 import { paginationConfig } from '@/constant';
-import { Popconfirm } from "antd";
-import { useState } from "react";
-import { Drawer } from "antd";
-import StudentInformation from './components/StudentInformation'
+import { Popconfirm } from 'antd';
+import { useState } from 'react';
+import StudentInformation from './components/StudentInformation';
+import AddClass from './components/AddClass';
 
+const ClassManagement = () => {
+  const [modalType, setModalType] = useState<string>('add');
+  const [current, setCurrent] = useState<RoomItem | null>(null);
 
-const ClassManagement=()=>{
-    const [modalType, setModalType] = useState<string>('add');
-    const [modalVisible, setModalVisible] = useState<boolean>(false);
-    const [current, setCurrent] = useState<RoomItem | null>(null);
-    const [form, setForm] = useState<FormInstance<any>>();
+  const [visible, setVisible] = useState(false);
 
-    const getModelTitle = () => {
-        if (modalType === 'preview') {
-            return '作息时间表预览';
-        }
-        if (modalType === 'classReset') {
-            return '节次维护';
-        }
-        if (current) {
-            return '编辑信息';
-        }
-        return '新增';
-    };
-    const handleOperation = (type: string) => {
-        // eslint-disable-next-line @typescript-eslint/no-unused-expressions
-        type === 'add' ? setCurrent(null) : '';
-        setModalType(type);
-        setModalVisible(true);
-    };
+  const showDrawer = () => {
+    setVisible(true);
+  };
 
-    const handleEdit = (data: RoomItem) => {
-        setModalType('add');
-        setCurrent(data);
-        getModelTitle();
-        setModalVisible(true);
-    };
-    const handleSubmit = async () => {
-        try {
-            const values = await form?.validateFields();
-            console.log('Success:', values);
-        } catch (errorInfo) {
-            console.log('Failed:', errorInfo);
-        }
-    };
+  const onClose = () => {
+    setVisible(false);
+  };
+  const getModelTitle = () => {
+    if (modalType === 'preview') {
+      return '作息时间表预览';
+    }
+    if (modalType === 'classReset') {
+      return '节次维护';
+    }
+    if (current) {
+      return '编辑信息';
+    }
+    return '新增';
+  };
 
+  const handleEdit = (data: RoomItem) => {
+    setModalType('add');
+    setCurrent(data);
+    getModelTitle();
+  };
 
-
-    const columns: ProColumns<RoomItem>[] = [
-        {
-            title: '序号',
-            dataIndex: 'index',
-            valueType: 'index',
-            width: 48,
-        },
-        {
-            title: '班级名称',
-            dataIndex: 'BJMC',
-            align: 'center',
-        },
-        {
-            title: '上课地点',
-            dataIndex: 'SKDD',
-            align: 'center',
-        },
-        {
-            title: '授课老师',
-            dataIndex: 'SKLS',
-            align: 'center',
-        },
-        {
-            title: `助教老师`,
-            dataIndex: 'ZJLS',
-            align: 'center',
-            ellipsis: true,
-            // render:(dom,index)=>{
-            //     console.log(index.ZJLS)
-            //     return(
-            //         <>
-            //        {
-            //            index.ZJLS?.map((item)=>{
-            //                return (
-            //                ` ${item};`
-            //                )
-            //            })
-            //        }
-            //         </>
-            //     )
-            // }
-        },
-        {
-            title: '学生人数',
-            dataIndex: 'XSRS',
-            align: 'center',
-            render: (dom) => {
-                return (
-                     <a >
-                         <StudentInformation dom={dom}/>
-                     </a>
-                )
-            }
-
-        },
-        {
-            title: '简介',
-            dataIndex: 'JJ',
-            align: 'center',
-            ellipsis: true,
-        },
-        {
-            title: '状态',
-            dataIndex: 'ZT',
-            align: 'center',
-            width: 220,
-        },
-        {
-            title: '操作',
-            valueType: 'option',
-            width: 100,
-            render: (_, record) => (
-                <>
-                    <a onClick={() => handleEdit(record)} >编辑</a>
-                    <Divider type="vertical" />
-                    <Popconfirm title="确定删除吗？" okText="是" cancelText="否">
-                        <a >删除</a>
-                    </Popconfirm>
-                </>
-            ),
-            align: 'center',
-        },
-    ];
-
-    return (
+  const columns: ProColumns<RoomItem>[] = [
+    {
+      title: '序号',
+      dataIndex: 'index',
+      valueType: 'index',
+      width: 48,
+    },
+    {
+      title: '班级名称',
+      dataIndex: 'BJMC',
+      align: 'center',
+    },
+    {
+      title: '上课地点',
+      dataIndex: 'SKDD',
+      align: 'center',
+    },
+    {
+      title: '授课老师',
+      dataIndex: 'SKLS',
+      align: 'center',
+    },
+    {
+      title: `助教老师`,
+      dataIndex: 'ZJLS',
+      align: 'center',
+      ellipsis: true,
+      // render:(dom,index)=>{
+      //     console.log(index.ZJLS)
+      //     return(
+      //         <>
+      //        {
+      //            index.ZJLS?.map((item)=>{
+      //                return (
+      //                ` ${item};`
+      //                )
+      //            })
+      //        }
+      //         </>
+      //     )
+      // }
+    },
+    {
+      title: '学生人数',
+      dataIndex: 'XSRS',
+      align: 'center',
+      render: (dom) => {
+        return (
+          <a>
+            <StudentInformation dom={dom} />
+          </a>
+        );
+      },
+    },
+    {
+      title: '简介',
+      dataIndex: 'JJ',
+      align: 'center',
+      ellipsis: true,
+    },
+    {
+      title: '状态',
+      dataIndex: 'ZT',
+      align: 'center',
+      width: 220,
+    },
+    {
+      title: '操作',
+      valueType: 'option',
+      width: 100,
+      render: (_, record) => (
         <>
-            <PageContainer cls={styles.roomWrapper} >
-                <ProTable<RoomItem>
-                    columns={columns}
-                    search={false}
-                    options={{
-                        setting: false,
-                        fullScreen: false,
-                        density: false,
-                        reload: false,
-                    }}
-                    pagination={paginationConfig}
-                    dataSource={listData}
-                    toolBarRender={() => [
-                        <Button
-                            style={{ background: theme.primaryColor, borderColor: theme.primaryColor }}
-                            type="primary"
-                            key="add"
-                            onClick={() => handleOperation('add')}
-                        >
-                            新增课程
-                         </Button>,
-                    ]}
-                />
-                  <Drawer
-                    title={getModelTitle()}
-                    destroyOnClose
-                    placement="right"
-                    width={modalType === 'classReset' ? '30vw' : '30vw'}
-                    visible={modalVisible}
-                    onClose={() => setModalVisible(false)}
-                    footer={
-                        modalType === 'add'
-                            ? [
-                                <Button key="back" onClick={() => setModalVisible(false)}>
-                                    取消
-                        </Button>,
-                                <Button key="submit" type="primary" onClick={handleSubmit}>
-                                    确定
-                        </Button>,
-                            ]
-                            : null
-                    }
-                    maskClosable={false}
-                    bodyStyle={{
-                        maxHeight: '65vh',
-                        overflowY: 'auto',
-                    }}
-                >
-                 
-                </Drawer>
-            </PageContainer>
+          <a onClick={() => handleEdit(record)}>编辑</a>
+          <Divider type="vertical" />
+          <Popconfirm title="确定删除吗？" okText="是" cancelText="否">
+            <a>删除</a>
+          </Popconfirm>
         </>
+      ),
+      align: 'center',
+    },
+  ];
 
-    )
-}
-export default ClassManagement
+  return (
+    <>
+      <PageContainer cls={styles.roomWrapper}>
+        <ProTable<RoomItem>
+          columns={columns}
+          search={false}
+          options={{
+            setting: false,
+            fullScreen: false,
+            density: false,
+            reload: false,
+          }}
+          pagination={paginationConfig}
+          dataSource={listData}
+          toolBarRender={() => [
+            <Button
+              style={{ background: theme.primaryColor, borderColor: theme.primaryColor }}
+              type="primary"
+              key="add"
+              onClick={() => showDrawer()}
+            >
+              新增课程
+            </Button>,
+          ]}
+        />
+        <AddClass visible={visible} onClose={onClose} />
+      </PageContainer>
+    </>
+  );
+};
+export default ClassManagement;
