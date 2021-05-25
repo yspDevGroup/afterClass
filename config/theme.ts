9ae1a6b17298cee486db81c6470f7e58a62f34e8
SissleLynly <861271413@qq.com>
2021-05-25 16:04:53 +0800

    根据UI调整主题,针对antd色值覆盖

--- config/theme.ts
+++ config/theme.ts
@@ -10,19 +10,19 @@ const paddingSize = 16;
 
 export default {
   'primary-color': 'rgba(81, 208, 129, 1)',
-  'btn-primary-bg': 'rgba(72, 132, 255, 1)',
+  // 'btn-primary-bg': 'rgba(72, 132, 255, 1)',
   'layout-body-background': 'rgba(251, 251, 251, 1)', // 页面背景底色
   'success-color': 'rgba(82, 196, 26, 1)', // 成功色
   'warning-color': 'rgba(255, 178, 24, 1)', // 警告色
   'error-color': 'rgba(234, 17, 28, 1)', // 错误色
   'border-color-base': 'rgba(228, 228, 228, 1)', // 边框色
   'btn-default-border': 'rgba(160, 160, 160, 1)', // btn边框色
-  'item-hover-bg':'rgba(81, 208, 129, 0.7)',
-  'link-hover-color':'rgba(81, 208, 129, 0.7)',
+  'btn-primary-bg': 'rgba(81, 208, 129, 1)', // btn 主题色
+  'item-hover-bg': 'rgba(81, 208, 129, 0.7)', // 内容hover颜色
   'link-color': 'rgba(72, 132, 255, 1)', // link 链接颜色
   'text-color': 'rgba(51, 51, 51, 1)', // 主文本色
   'text-color-secondary': 'rgba(102, 102, 102, 1)', // 二级文本色
-  'normal-color':'rgba(153, 153, 153, 1)', // 正常按钮颜色，提示文本颜色
+  'normal-color': 'rgba(153, 153, 153, 1)', // 正常按钮颜色，提示文本颜色
   'disabled-color': 'rgba(187, 187, 187, 1)', // 禁用、失效色
   'divider-color': 'rgba(238, 238, 238, 1)', // 分割线
   'font-size-base': `${mainFontSize}px`, // 主字号
