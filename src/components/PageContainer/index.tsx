38344697898599156dea091ab910f15ae85453b2
SissleLynly <861271413@qq.com>
2021-06-04 12:31:41 +0800

    调整一级菜单下不显示面包屑的问题

--- src/components/PageContainer/index.tsx
+++ src/components/PageContainer/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { PageContainer } from '@ant-design/pro-layout';
 import RightContent from '@/components/RightContent';
 import styles from './index.less';
+import { Link } from 'umi';
 
 type pageProp = { children?: React.ReactNode; cls?: string };
 const PageContain = ({ children, cls }: pageProp) => {
@@ -10,6 +11,24 @@ const PageContain = ({ children, cls }: pageProp) => {
       className={`${styles.customPageHeader} ${cls}`}
       header={{
         title: '',
+        breadcrumbRender: (props, defaultDom) => {
+          const { breadcrumb, currentMenu } = props;
+          console.log(props);
+
+          if (breadcrumb?.routes === undefined && currentMenu.name !== '首页') {
+            return <div className="ant-breadcrumb">
+              <span>
+                <span className="ant-breadcrumb-link">
+                  <Link to={currentMenu.path}>
+                    {currentMenu.name}
+                  </Link>
+                </span>
+              </span>
+            </div>
+          }
+
+          return <>{defaultDom}</>
+        },
         extra: <RightContent />,
       }}
       fixedHeader
