c3742831ad6ca9a051900ad15d88055c58aa815d
tianxinxin915 <571556548@qq.com>
2021-06-15 11:09:17 +0800

    修改了了list组件，增加了首页的小圆点和链接跳转的范围

--- src/components/ListComponent/index.tsx
+++ src/components/ListComponent/index.tsx
@@ -2,13 +2,14 @@
  * @description: 
  * @author: txx
  * @Date: 2021-05-31 10:24:05
- * @LastEditTime: 2021-06-09 17:56:00
+ * @LastEditTime: 2021-06-15 10:12:24
  * @LastEditors: txx
  */
 
 import type { ListData, ListItem, ListType } from "./data";
 import { List } from 'antd';
 import styles from "./index.less";
+import { Link } from "umi";
 
 
 const NewsList = (props: { data: ListItem[], type: ListType }) => {
@@ -17,38 +18,38 @@ const NewsList = (props: { data: ListItem[], type: ListType }) => {
     <List
       dataSource={data}
       renderItem={(v) => (
-        <List.Item.Meta
-          avatar={v.img ? <img width="110" height="70" alt={v.title} src={v.img} /> : ''}
-          title={<div className={styles.TitleRow}>
-            <div className={styles.Title} >
-              <a href={v.link}>
+        <Link to={v.link!}>
+          <List.Item.Meta
+            avatar={v.img ? <img width="110" height="70" alt={v.title} src={v.img} /> : ''}
+            title={<div className={styles.TitleRow}>
+              <div className={styles.Title} >
                 {v.title}
-              </a>
-            </div>
-            <div className={styles.TitleRight}>
-              {v.titleRight ?
-                <span style={{ color: v.titleRight.color }} >
-                  {v.titleRight.text}
-                </span>
-                : ''}
-            </div>
-          </div>}
-          description={<>{v.desc?.map((item, index) => {
-            return <div className={styles.descRow} key={`${v.title}${index.toString()}`}>
-              <div className={styles.descleft}>
-                {item.left.map((t, i) => {
-                  return <span key={t} className={styles.descleftspan}>
-                    {i === 0 ? '' : <span className={styles.descleftInspan}>|</span>}
-                    {t}
+              </div>
+              <div className={styles.TitleRight}>
+                {v.titleRight ?
+                  <span style={{ color: v.titleRight.color }} >
+                    {v.titleRight.text}
                   </span>
-                })}
+                  : ''}
               </div>
-              <div className={styles.descright}>
-                {item.right}
+            </div>}
+            description={<>{v.desc?.map((item, index) => {
+              return <div className={styles.descRow} key={`${v.title}${index.toString()}`}>
+                <div className={styles.descleft}>
+                  {item.left.map((t, i) => {
+                    return <span key={t} className={styles.descleftspan}>
+                      {i === 0 ? '' : <span className={styles.descleftInspan}>|</span>}
+                      {t}
+                    </span>
+                  })}
+                </div>
+                <div className={styles.descright}>
+                  {item.right}
+                </div>
               </div>
-            </div>
-          })}</>}
-        />
+            })}</>}
+          />
+        </Link>
 
       )}
     />
