ac3ec34ab52a15617c8f4ce1b21765ea96a544b6
tangjunjun <2413361543@qq.com>
2021-06-24 11:57:03 +0800

    完善了日历获取课程的方法

--- src/pages/Manager/BasicalSettings/PeriodMaintenance/components/TimePeriodForm.tsx
+++ src/pages/Manager/BasicalSettings/PeriodMaintenance/components/TimePeriodForm.tsx
@@ -49,7 +49,7 @@ const TimePeriodForm = (props: PropsType) => {
       rules: [{ required: true, message: '请填写名称' }],
       fieldProps: {
         autocomplete: 'off',
-        placeholder: '如：第一节课',
+        placeholder: currentStatus === 'schedule' ?'如：第一节课':'',
       },
     },
     {
--- src/pages/Parent/Homepage/Study/ClassCalendar/index.tsx
+++ src/pages/Parent/Homepage/Study/ClassCalendar/index.tsx
@@ -1,10 +1,9 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import dayjs from 'dayjs';
 import { Calendar } from 'react-h5-calendar';
 import styles from './index.less';
 import ListComponent from '@/components/ListComponent';
 import PromptInformation from './components/PromptInformation';
-import { courseArr } from '../mock';
 import { DateRange, Week } from '@/utils/Timefunction';
 import myContext from '../../myContext';
 import moment from 'moment';
@@ -15,32 +14,65 @@ const ClassCalendar = () => {
   const [cDay, setCDay] = useState<string>(dayjs().format('M月D日'));
   const [course, setCourse] = useState<any>();
   const { weekSchedule } = useContext(myContext);
-  // console.log('weekSchedule', weekSchedule[0].KHBJSJ)
-  // console.log(DateRange('2021/06/01', '2021/06/30'))
-  // console.log(Week('2021/06/24'))
+  const [dates, setDates] = useState<any[]>([]);
+  const [courseArr, setCourseArr] = useState<any>({})
+
   // 后台返回的周数据的遍历
- const arry= weekSchedule.map((item: any) => {
+  const yy = {};
+  const arrys = weekSchedule.map((item: any) => {
     const arry: any[] = [];
+    item.xx = {
+      type: 'picList',
+      cls: 'picList',
+      list: [
+        {
+          title: item.KHBJSJ.KHKCSJ.KCMC,
+          img: item.KHBJSJ.KCTP ? item.KHBJSJ.KCTP : item.KHBJSJ.KHKCSJ.KCTP,
+          link: `/parent/home/courseDetails?id=${item.KHBJSJ.id}&type=true`,
+          desc: [
+            {
+              left: [`课程时段：${item.XXSJPZ.KSSJ}-${item.XXSJPZ.JSSJ}`],
+            },
+            {
+              left: [`上课地点：${item.KHBJSJ.XQName}`],
+            },
+          ],
+        },
+      ]
+    };
     if (item.KHBJSJ.KKRQ && item.KHBJSJ.JKRQ) {
       const res = DateRange(moment(item.KHBJSJ.KKRQ).format('YYYY/MM/DD'), moment(item.KHBJSJ.JKRQ).format('YYYY/MM/DD'));
       res.map((arrye: any) => {
         const erry = Week(moment(arrye).format('YYYY/MM/DD'));
         if (erry === item.WEEKDAY) {
           arry.push(arrye);
+          yy[arrye] = item.xx;
         }
       })
-    }else{
-      const res =DateRange(moment(item.KHBJSJ.KHKCSJ.KKRQ).format('YYYY/MM/DD'),moment(item.KHBJSJ.KHKCSJ.JKRQ).format('YYYY/MM/DD'));
+    } else {
+      const res = DateRange(moment(item.KHBJSJ.KHKCSJ.KKRQ).format('YYYY/MM/DD'), moment(item.KHBJSJ.KHKCSJ.JKRQ).format('YYYY/MM/DD'));
       res.map((arrye: any) => {
         const erry = Week(moment(arrye).format('YYYY/MM/DD'));
         if (erry === item.WEEKDAY) {
           arry.push(arrye);
+          yy[arrye] = item.xx;
         }
       })
     }
+    setCourseArr(yy);
     return arry;
   })
-  console.log('arry',arry)
+  useEffect(() => {
+    const eray: any[] = []
+    arrys.map((item: any) => {
+      item.map((items: any) => {
+        eray.push({ date: items })
+      })
+    })
+    setDates(eray);
+  }, [])
+console.log('courseArr',courseArr)
+
   return (
     <div className={styles.schedule}>
       <span className={styles.today} onClick={() => {
@@ -51,11 +83,7 @@ const ClassCalendar = () => {
       </span>
       <Calendar
         showType={'week'}
-        markDates={[
-          { date: '2021-6-7' },
-          { date: '2021-6-9' },
-          { date: '2021-6-10' },
-        ]}
+        markDates={dates}
         onDateClick={(date: { format: (arg: string) => any; }) => {
           setDay(date.format('YYYY-MM-DD'));
           setCDay(date.format('M月D日'));
