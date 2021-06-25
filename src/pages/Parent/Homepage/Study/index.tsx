7d1ee4c4418d22f8f00d6bcbab883de79cfdd2df
tangjunjun <2413361543@qq.com>
2021-06-25 08:42:02 +0800

    修改了在学课程的数据获取

--- src/pages/Parent/Homepage/Study/index.tsx
+++ src/pages/Parent/Homepage/Study/index.tsx
@@ -9,40 +9,46 @@ import myContext from '../myContext';
 
 const Study = () => {
   // 从日历中获取的时间
-  const [datedata, setDatedata] = useState<any[]>([]);
+  const [datedata, setDatedata] = useState<any>();
   // 获取列表数据
   const { weekSchedule, yxkc } = useContext(myContext);
   // 在学课程数据
   const myDate = new Date();
   const nowdata = new Date(moment(myDate.toLocaleDateString()).format('YYYY-MM-DD'));
   const Timetable = [];
-  for (let i = 0; i < datedata.length; i+=1) {
-    if (new Date(datedata[i]) < nowdata) {
-      Timetable.push(datedata[i]);
-    }
-  }
-  weekSchedule.map((record: any) => {
+
+
+  yxkc?.map((record:any)=>{
+     if(datedata && datedata[record.id]){
+        console.log(datedata[record.id].dates)
+        datedata[record.id].dates.map((item:any)=>{
+            if(new Date(item)<nowdata){
+              Timetable.push(item);
+            }
+        })
       record.zxkc = {
-        type: 'list',
-        cls: 'list',
-        list: [
-          {
-            id: record.KHBJSJ.id,
-            title: record.KHBJSJ.KHKCSJ.KCMC,
-            link: `/parent/home/courseDetails?classid=${record.KHBJSJ.id}&type=false`,
-            desc: [
-              {
-                left: ['每周一', `${(record.XXSJPZ.KSSJ).substring(0,5)}-${(record.XXSJPZ.JSSJ).substring(0,5)}`,`${record.FJSJ.FJMC}` ],
-              },
-              {
-                left: [`共${record.KHBJSJ.KSS}`,`已学${Timetable.length}课时`],
-              },
-            ],
-          },
-        ],
-        noDataText: '暂无课程',
-      }
+          type: 'list',
+          cls: 'list',
+          list: [
+            {
+              id: record.id,
+              title:record.KHKCSJ.KCMC,
+              link: `/parent/home/courseDetails?classid=${record.id}&type=false`,
+              desc: [
+                {
+                  left: ['每周一', `${(datedata[record.id].courseInfo.XXSJPZ.KSSJ).substring(0,5)}-${(datedata[record.id].courseInfo.XXSJPZ.JSSJ).substring(0,5)}`,`${datedata[record.id].courseInfo.FJSJ.FJMC}` ],
+                },
+                {
+                  left: [`共${record.KSS}`,`已学${Timetable.length}课时`],
+                },
+              ],
+            },
+          ],
+          noDataText: '暂无课程',
+        }  
+     }
   })
+
   return <div className={styles.studyPage}>
     <DisplayColumn
       type="icon"
@@ -57,7 +63,7 @@ const Study = () => {
     <div className={styles.funWrapper}>
       <div className={styles.titleBar}>{`在学课程 ${yxkc?.length}`}</div>
       {
-        weekSchedule.map((item: any)=>{
+        yxkc?.map((item: any)=>{
         return   <ListComponent listData={item.zxkc} />
         })
       }
