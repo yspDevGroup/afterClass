aa1c825a57faa9d3352852ddaa9e2a6a2514375a
tangjunjun <2413361543@qq.com>
2021-06-26 18:19:31 +0800

    修改页面数据报错问题

--- src/pages/Parent/Homepage/Home/Pages/CourseDetails/index.tsx
+++ src/pages/Parent/Homepage/Home/Pages/CourseDetails/index.tsx
@@ -5,15 +5,13 @@ import { Button, message, Radio, Tooltip } from 'antd';
 import React, { useEffect, useState } from 'react';
 import { Link, useModel } from 'umi';
 import styles from './index.less';
-import { TimetableList } from './mock';
 import type { KcDetailType } from './data'
 import { getDetailsKHKCSJ } from '@/services/after-class/khkcsj';
-import {currentUser} from '@/services/after-class/user'
 import { getQueryString } from '@/utils/utils';
 import { getAllKHXSCQ } from '@/services/after-class/khxscq';
 import { DateRange, Week } from '@/utils/Timefunction';
 import moment from 'moment';
-import { getKHBJSJ } from '@/services/after-class/khbjsj';
+
 
 
 
@@ -29,8 +27,8 @@ const CourseDetails: React.FC = () => {
   const { currentUser } = initialState || {};
   const [timetableList, setTimetableList] = useState<any[]>()
   const hrefs = window.location.href;
-  let courseid: any ;
-  let classid: any ;
+  let courseid: any;
+  let classid: any;
   let valueKey = '';
 
   useEffect(() => {
@@ -97,7 +95,7 @@ const CourseDetails: React.FC = () => {
   if (hrefs.indexOf('classid') === -1) {
     courseid = hrefs.split('courseid=')[1].split('&')[0];
     valueKey = 'true';
-  }else{
+  } else {
     courseid = getQueryString('courseid');
     classid = getQueryString('classid');
     valueKey = 'false';
@@ -106,13 +104,7 @@ const CourseDetails: React.FC = () => {
   useEffect(() => {
     (async () => {
       const result = await getDetailsKHKCSJ(courseid);
-      const resultUser = await currentUser();
-
-      if (resultUser.status === 'ok') {
-        setStudent(resultUser.data?.info.username)
-      } else {
-        message.error(result.message);
-      }
+      setStudent(currentUser?.username);
       if (result.status === 'ok') {
         setKcDetail(result.data)
         setFY(result.data.KHBJSJs[0].FY)
@@ -121,7 +113,7 @@ const CourseDetails: React.FC = () => {
         message.error(result.message);
       }
     })();
-    const myDate = new Date().toLocaleDateString().slice(5,9);
+    const myDate = new Date().toLocaleDateString().slice(5, 9);
     setCurrentDate(myDate);
 
   }, [courseid]);
@@ -142,9 +134,9 @@ const CourseDetails: React.FC = () => {
     e.stopPropagation()
   }
   const submit = async () => {
-    if(BJ === undefined){
+    if (BJ === undefined) {
       message.error("请选择班级")
-    }else if (XY === false) {
+    } else if (XY === false) {
       message.info('请阅读并同意《课后服务协议》')
     }
     // const data ={
@@ -154,89 +146,89 @@ const CourseDetails: React.FC = () => {
   }
   const valueNames = `${BJ}+${FY}`;
   return <>
-  {
-    valueKey === 'true' ?
-    <div className={styles.CourseDetails}>
-    <div className={styles.wrap}>
-      {
-        KcDetail?.KCTP === 'string' || KcDetail?.KCTP === '' ?
-        <img src='https://i.postimg.cc/XXxH96fZ/noData.png' alt="" />:
-        <img src={KcDetail?.KCTP} alt="" />
-      }
-      <p className={styles.title}>{KcDetail?.KCMC}</p>
-      
-      <ul>
-        <li>上课时段：{KcDetail?.KKRQ}~{KcDetail?.JKRQ}</li>
-        <li>上课地点：本校</li>
-      </ul>
-      <p className={styles.title}>课程简介</p>
-      <p className={styles.content}>{KcDetail?.KCMS}</p>
-      <p className={styles.content} style={{ marginTop: '20px' }}>开设班级：</p>
-      <ul className={styles.classInformation}>
-        {
-          KcDetail?.KHBJSJs?.map((value: {BJMC: string,ZJS: string,FJS: string,KCSC: string,KHPKSJs: any})=>{
-            return<li>{value.BJMC}：总课时：{value.KCSC},
-             上课时间：{
-                value.KHPKSJs.map((values: {FJSJ: any,XXSJPZ: any,WEEKDAY: number})=>{
-                  const weeks = `周${'日一二三四五六'.charAt(values.WEEKDAY)}`;
-                  let kssj = '';
-                  if(values.XXSJPZ.KSSJ){
-                    values.XXSJPZ.KSSJ.split(':')
-                    kssj = `${values.XXSJPZ.KSSJ.split(':')[0]}:${values.XXSJPZ.KSSJ.split(':')[1]}`
-                  }
-                  let jssj = '';
-                  if(values.XXSJPZ.JSSJ){
-                    values.XXSJPZ.JSSJ.split(':')
-                    jssj = `${values.XXSJPZ.JSSJ.split(':')[0]}:${values.XXSJPZ.JSSJ.split(':')[1]}`
-                  }
-                  return<span>{weeks}{kssj}-{jssj},</span>
-                })
-            }
-            上课地点：{
-              value.KHPKSJs.map((values: {FJSJ: any})=>{
-                return<span>{values.FJSJ.FJMC},</span>
-              })
-            }
-            班主任：{value.ZJS},副班：{value.FJS}。</li>
-          })
-        }
-      </ul>
-    </div>
-    <div className={styles.footer}>
-      <button className={styles.btn} onClick={onclick}>立即报名</button>
-    </div>
     {
-      state === true ?
-        <div className={styles.payment} onClick={onclose}>
-          <div onClick={onchanges}>
+      valueKey === 'true' ?
+        <div className={styles.CourseDetails}>
+          <div className={styles.wrap}>
+            {
+              KcDetail?.KCTP === 'string' || KcDetail?.KCTP === '' ?
+                <img src='https://i.postimg.cc/XXxH96fZ/noData.png' alt="" /> :
+                <img src={KcDetail?.KCTP} alt="" />
+            }
             <p className={styles.title}>{KcDetail?.KCMC}</p>
-            <p className={styles.price}><span>￥{FY}</span><span>/学期</span></p>
-            <p className={styles.title} style={{ fontSize: '14px' }}>班级</p>
-            <Radio.Group onChange={onBJChange} defaultValue={valueNames}>
+
+            <ul>
+              <li>上课时段：{KcDetail?.KKRQ}~{KcDetail?.JKRQ}</li>
+              <li>上课地点：本校</li>
+            </ul>
+            <p className={styles.title}>课程简介</p>
+            <p className={styles.content}>{KcDetail?.KCMS}</p>
+            <p className={styles.content} style={{ marginTop: '20px' }}>开设班级：</p>
+            <ul className={styles.classInformation}>
               {
-                KcDetail?.KHBJSJs?.map((value: {BJMC: string,id: string,FJS: string,FY: string}) => {
-                  const text = `${value.BJMC}已有12人报名，共50个名额`;
-                  const valueName = `${value.id}+${value.FY}`;
-                  return <div className={styles.BjInformation}>
-                    <Tooltip placement="bottomLeft" title={text}>
-                      <Radio.Button value={valueName}>{value.BJMC}</Radio.Button>
-                    </Tooltip>
-                  </div>
+                KcDetail?.KHBJSJs?.map((value: { BJMC: string, ZJS: string, FJS: string, KCSC: string, KHPKSJs: any }) => {
+                  return <li>{value.BJMC}：总课时：{value.KCSC},
+             上课时间：{
+                      value.KHPKSJs.map((values: { FJSJ: any, XXSJPZ: any, WEEKDAY: number }) => {
+                        const weeks = `周${'日一二三四五六'.charAt(values.WEEKDAY)}`;
+                        let kssj = '';
+                        if (values.XXSJPZ.KSSJ) {
+                          values.XXSJPZ.KSSJ.split(':')
+                          kssj = `${values.XXSJPZ.KSSJ.split(':')[0]}:${values.XXSJPZ.KSSJ.split(':')[1]}`
+                        }
+                        let jssj = '';
+                        if (values.XXSJPZ.JSSJ) {
+                          values.XXSJPZ.JSSJ.split(':')
+                          jssj = `${values.XXSJPZ.JSSJ.split(':')[0]}:${values.XXSJPZ.JSSJ.split(':')[1]}`
+                        }
+                        return <span>{weeks}{kssj}-{jssj},</span>
+                      })
+                    }
+            上课地点：{
+                      value.KHPKSJs.map((values: { FJSJ: any }) => {
+                        return <span>{values.FJSJ.FJMC},</span>
+                      })
+                    }
+            班主任：{value.ZJS},副班：{value.FJS}。</li>
                 })
               }
-            </Radio.Group>
-            <Radio 
-              className={styles.agreement}
-              onChange={onXYChange}
-            >  <p>我已阅读并同意<a href=''>《课后帮服务协议》</a></p></Radio>
-            {
-              XY === false || BJ === undefined ?
-              <Button className={styles.submit} onClick={submit} >确定并付款</Button>:
-              <Link to={`/parent/mine/orderDetails?id=${courseid}&type=false`}><Button className={styles.submit} onClick={submit} >确定并付款</Button></Link>
-            }
+            </ul>
           </div>
-        </div> : ''
-    }
+          <div className={styles.footer}>
+            <button className={styles.btn} onClick={onclick}>立即报名</button>
+          </div>
+          {
+            state === true ?
+              <div className={styles.payment} onClick={onclose}>
+                <div onClick={onchanges}>
+                  <p className={styles.title}>{KcDetail?.KCMC}</p>
+                  <p className={styles.price}><span>￥{FY}</span><span>/学期</span></p>
+                  <p className={styles.title} style={{ fontSize: '14px' }}>班级</p>
+                  <Radio.Group onChange={onBJChange} defaultValue={valueNames}>
+                    {
+                      KcDetail?.KHBJSJs?.map((value: { BJMC: string, id: string, FJS: string, FY: string }) => {
+                        const text = `${value.BJMC}已有12人报名，共50个名额`;
+                        const valueName = `${value.id}+${value.FY}`;
+                        return <div className={styles.BjInformation}>
+                          <Tooltip placement="bottomLeft" title={text}>
+                            <Radio.Button value={valueName}>{value.BJMC}</Radio.Button>
+                          </Tooltip>
+                        </div>
+                      })
+                    }
+                  </Radio.Group>
+                  <Radio
+                    className={styles.agreement}
+                    onChange={onXYChange}
+                  >  <p>我已阅读并同意<a href=''>《课后帮服务协议》</a></p></Radio>
+                  {
+                    XY === false || BJ === undefined ?
+                      <Button className={styles.submit} onClick={submit} >确定并付款</Button> :
+                      <Link to={`/parent/mine/orderDetails?id=${courseid}&type=false`}><Button className={styles.submit} onClick={submit} >确定并付款</Button></Link>
+                  }
+                </div>
+              </div> : ''
+          }
 
         </div> :
         <div className={styles.CourseDetails2}>
@@ -276,13 +268,13 @@ const CourseDetails: React.FC = () => {
                 })
               }
             </div>
-          
-     
-    </div>
-  </div>
-  }
+
+
+          </div>
+        </div>
+    }
   </>
-  
+
 };
 
 export default CourseDetails
