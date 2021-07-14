8aafba8fef54404318297f0176995d0ea79aefc1
Liujin <1287784468@qq.com>
2021-07-14 17:03:17 +0800

    修改课程详情主班副班在IOS加载不出来的问题

--- src/pages/Parent/Homepage/Home/Pages/CourseDetails/index.tsx
+++ src/pages/Parent/Homepage/Home/Pages/CourseDetails/index.tsx
@@ -1,6 +1,6 @@
 import { Button, Radio } from 'antd';
 import React, { useEffect, useState, useRef } from 'react';
-import { useModel, Link, } from 'umi';
+import { useModel, Link } from 'umi';
 import styles from './index.less';
 import { getKHKCSJ } from '@/services/after-class/khkcsj';
 import { enHenceMsg, getQueryString } from '@/utils/utils';
@@ -10,7 +10,6 @@ import WWOpenDataCom from '@/pages/Manager/ClassManagement/components/WWOpenData
 import { initWXAgentConfig, initWXConfig } from '@/utils/wx';
 import noPic from '@/assets/noPic.png';
 import GoBack from '@/components/GoBack';
-
 
 const CourseDetails: React.FC = () => {
   const { initialState } = useModel('@@initialState');
@@ -23,22 +22,27 @@ const CourseDetails: React.FC = () => {
   const [classDetail, setClassDetail] = useState<any>();
   const [kaiguan, setKaiguan] = useState<boolean>(true);
   const [fk, setFk] = useState<boolean>(false);
+  const [isLoading, setIsLoading] = useState(false);
   const linkRef = useRef<HTMLAnchorElement | null>(null);
   const courseid = getQueryString('courseid');
   const index = getQueryString('index');
   const myDate: Date = new Date();
-  const children = currentUser?.subscriber_info?.children || [{
-    student_userid: currentUser?.UserId,
-    njId: '1',
-    name: currentUser?.username
-  }];
+  const children = currentUser?.subscriber_info?.children || [
+    {
+      student_userid: currentUser?.UserId,
+      njId: '1',
+      name: currentUser?.username,
+    },
+  ];
 
   const changeStatus = (ind: number, data?: any) => {
     const detail = data || KcDetail;
     const current = detail.KHBJSJs![ind];
     const start = current.BMKSSJ ? current.BMKSSJ : detail.BMKSSJ;
     const end = current.BMKSSJ ? current.BMJSSJ : detail.BMJSSJ;
-    const enAble = myDate >= new Date(moment(start).format('YYYY/MM/DD')) && myDate <= new Date(moment(end).format('YYYY/MM/DD'));
+    const enAble =
+      myDate >= new Date(moment(start).format('YYYY/MM/DD')) &&
+      myDate <= new Date(moment(end).format('YYYY/MM/DD'));
     setFY(current.FY);
     setBJ(current.id);
     setFk(!enAble);
@@ -50,6 +54,7 @@ const CourseDetails: React.FC = () => {
       }
       if (await initWXConfig(['checkJsApi'])) {
         await initWXAgentConfig(['checkJsApi']);
+        setIsLoading(true);
       }
     })();
   }, []);
@@ -60,7 +65,7 @@ const CourseDetails: React.FC = () => {
         const result = await getKHKCSJ({
           kcId: courseid,
           bjzt: '已发布',
-          njId: children[0].njId
+          njId: children[0].njId,
         });
         if (result.status === 'ok') {
           if (result.data) {
@@ -72,154 +77,205 @@ const CourseDetails: React.FC = () => {
             setKaiguan(btnEnable);
           }
         } else {
-          enHenceMsg(result.message)
+          enHenceMsg(result.message);
         }
       })();
     }
   }, [courseid]);
 
   useEffect(() => {
-    if (orderInfo)
-      linkRef.current?.click();
-  }, [orderInfo])
+    if (orderInfo) linkRef.current?.click();
+  }, [orderInfo]);
   const onBJChange = (e: any) => {
     const bjId = e.target.value.split('+')[0];
     setBJ(bjId);
     setFY(e.target.value.split('+')[1]);
-  }
-  const onchanges = (e: { stopPropagation: () => void; }) => {
+  };
+  const onchanges = (e: { stopPropagation: () => void }) => {
     e.stopPropagation();
-  }
+  };
   const submit = async () => {
     const bjInfo = KcDetail.KHBJSJs.find((item: any) => {
-      return item.id === BJ
+      return item.id === BJ;
     });
     await setClassDetail(bjInfo);
     const data: API.CreateKHXSDD = {
-      'XDSJ': (new Date).toISOString(),
-      "ZFFS": "线上支付",
-      "DDZT": "待付款",
-      "DDFY": FY!,
-      "XSId": children[0].student_userid!,
-      "XSXM": children[0].name!,
-      "KHBJSJId": BJ!,
+      XDSJ: new Date().toISOString(),
+      ZFFS: '线上支付',
+      DDZT: '待付款',
+      DDFY: FY!,
+      XSId: children[0].student_userid!,
+      XSXM: children[0].name!,
+      KHBJSJId: BJ!,
     };
     const res = await createKHXSDD(data);
     if (res.status === 'ok') {
       setOrderInfo(res.data);
     } else {
-      enHenceMsg(res.message)
+      enHenceMsg(res.message);
     }
-  }
+  };
   // 房间数据去重
   const tempfun = (arr: any) => {
     const fjname: string[] = [];
     arr.forEach((item: any) => {
       if (!fjname.includes(item.FJSJ.FJMC)) {
-        fjname.push(item.FJSJ.FJMC)
+        fjname.push(item.FJSJ.FJMC);
       }
     });
     return fjname.map((values: any) => {
-      return <span>{values}</span>
-    })
-  }
+      return <span>{values}</span>;
+    });
+  };
   const butonclick = (ind: number) => {
     changeStatus(ind);
-  }
+  };
 
-  return <div className={styles.CourseDetails}>
-    {
-      index === 'all' ?
-        <GoBack title={'课程简介'} /> :
-        <GoBack title={'课程简介'} onclick='/parent/home?index=index' />
-    }
-    <div className={styles.wrap}>
-      {
-        KcDetail?.KCTP && KcDetail?.KCTP.indexOf('http') > -1 ?
-          <img src={KcDetail?.KCTP} alt="" style={{ marginBottom: '18px', height: '200px' }} /> :
-          <div style={{ padding: '10px', border: '1px solid #F7F7F7', textAlign: 'center', marginBottom: '18px' }}><img style={{ width: '180px', height: 'auto', marginBottom: 0 }} src={noPic} /></div>
-      }
-      <p className={styles.title}>{KcDetail?.KCMC}</p>
+  return (
+    <div className={styles.CourseDetails}>
+      {index === 'all' ? (
+        <GoBack title={'课程简介'} />
+      ) : (
+        <GoBack title={'课程简介'} onclick="/parent/home?index=index" />
+      )}
+      <div className={styles.wrap}>
+        {KcDetail?.KCTP && KcDetail?.KCTP.indexOf('http') > -1 ? (
+          <img src={KcDetail?.KCTP} alt="" style={{ marginBottom: '18px', height: '200px' }} />
+        ) : (
+          <div
+            style={{
+              padding: '10px',
+              border: '1px solid #F7F7F7',
+              textAlign: 'center',
+              marginBottom: '18px',
+            }}
+          >
+            <img style={{ width: '180px', height: 'auto', marginBottom: 0 }} src={noPic} />
+          </div>
+        )}
+        <p className={styles.title}>{KcDetail?.KCMC}</p>
 
-      <ul>
-        <li>上课时段：{moment(KcDetail?.KKRQ).format('YYYY.MM.DD')}~{moment(KcDetail?.JKRQ).format('YYYY.MM.DD')}</li>
-        <li>上课地点：本校</li>
-      </ul>
-      <p className={styles.title}>课程简介</p>
-      <p className={styles.content}>{KcDetail?.KCMS}</p>
-      <p className={styles.content} style={{ marginTop: '20px' }}>开设班级：</p>
-      <ul className={styles.classInformation}>
-        {
-          KcDetail?.KHBJSJs?.map((value: { BJMC: string, ZJS: string, FJS: string, KSS: number, KHPKSJs: any }) => {
-            return <li>{value.BJMC}：总课时：{value.KSS}课时,
-              上课时间：{
-                value.KHPKSJs.map((values: { FJSJ: any, XXSJPZ: any, WEEKDAY: number }) => {
-                  const weeks = `周${'日一二三四五六'.charAt(values.WEEKDAY)}`;
-                  let kssj = '';
-                  if (values.XXSJPZ.KSSJ) {
-                    values.XXSJPZ.KSSJ.split(':')
-                    kssj = `${values.XXSJPZ.KSSJ.split(':')[0]}:${values.XXSJPZ.KSSJ.split(':')[1]}`
-                  }
-                  let jssj = '';
-                  if (values.XXSJPZ.JSSJ) {
-                    values.XXSJPZ.JSSJ.split(':')
-                    jssj = `${values.XXSJPZ.JSSJ.split(':')[0]}:${values.XXSJPZ.JSSJ.split(':')[1]}`
-                  }
-                  return <span>{weeks}{kssj}-{jssj},</span>
-                })
+        <ul>
+          <li>
+            上课时段：{moment(KcDetail?.KKRQ).format('YYYY.MM.DD')}~
+            {moment(KcDetail?.JKRQ).format('YYYY.MM.DD')}
+          </li>
+          <li>上课地点：本校</li>
+        </ul>
+        <p className={styles.title}>课程简介</p>
+        <p className={styles.content}>{KcDetail?.KCMS}</p>
+        <p className={styles.content} style={{ marginTop: '20px' }}>
+          开设班级：
+        </p>
+        <ul className={styles.classInformation}>
+          {KcDetail?.KHBJSJs?.map(
+            (value: { BJMC: string; ZJS: string; FJS: string; KSS: number; KHPKSJs: any }) => {
+              if (isLoading) {
+                return (
+                  <li>
+                    {value.BJMC}：总课时：{value.KSS}课时, 上课时间：
+                    {value.KHPKSJs.map((values: { FJSJ: any; XXSJPZ: any; WEEKDAY: number }) => {
+                      const weeks = `周${'日一二三四五六'.charAt(values.WEEKDAY)}`;
+                      let kssj = '';
+                      if (values.XXSJPZ.KSSJ) {
+                        values.XXSJPZ.KSSJ.split(':');
+                        kssj = `${values.XXSJPZ.KSSJ.split(':')[0]}:${
+                          values.XXSJPZ.KSSJ.split(':')[1]
+                        }`;
+                      }
+                      let jssj = '';
+                      if (values.XXSJPZ.JSSJ) {
+                        values.XXSJPZ.JSSJ.split(':');
+                        jssj = `${values.XXSJPZ.JSSJ.split(':')[0]}:${
+                          values.XXSJPZ.JSSJ.split(':')[1]
+                        }`;
+                      }
+                      return (
+                        <span>
+                          {weeks}
+                          {kssj}-{jssj},
+                        </span>
+                      );
+                    })}
+                    上课地点：{tempfun(value.KHPKSJs)}，
+                    <span className={styles.bzrname}>
+                      班主任：{<WWOpenDataCom type="userName" openid={value.ZJS} />}
+                    </span>
+                    ,
+                    <span className={styles.bzrname}>
+                      副班：
+                      {value.FJS.split(',').map((item: any) => {
+                        return (
+                          <span style={{ marginRight: '5px' }}>
+                            <WWOpenDataCom type="userName" openid={item} />
+                          </span>
+                        );
+                      })}
+                      。
+                    </span>
+                  </li>
+                );
               }
-              上课地点：{tempfun(value.KHPKSJs)}，
-              <span className={styles.bzrname}>
-                班主任：{<WWOpenDataCom type="userName" openid={value.ZJS} />}
-              </span>
-              ,
-              <span className={styles.bzrname}>
-                副班：{
-                  value.FJS.split(',').map((item: any) => {
-                    return <span style={{ marginRight: '5px' }}><WWOpenDataCom type="userName" openid={item} /></span>
-                  })
-                }。
-              </span>
-            </li>
-          })
-        }
-      </ul>
-    </div>
-    <div className={styles.footer}>
-      {kaiguan ?
-        <button className={styles.btn} onClick={() => setstate(true)}>立即报名</button>
-        :
-        <button className={styles.btnes} >课程不在报名时间范围内</button>
-      }
-    </div>
-    {
-      state === true ?
+              return <></>;
+            },
+          )}
+        </ul>
+      </div>
+      <div className={styles.footer}>
+        {kaiguan ? (
+          <button className={styles.btn} onClick={() => setstate(true)}>
+            立即报名
+          </button>
+        ) : (
+          <button className={styles.btnes}>课程不在报名时间范围内</button>
+        )}
+      </div>
+      {state === true ? (
         <div className={styles.payment} onClick={() => setstate(false)}>
           <div onClick={onchanges}>
             <p className={styles.title}>{KcDetail?.KCMC}</p>
-            <p className={styles.price}><span>￥{FY}</span><span>/学期</span></p>
-            <p className={styles.title} style={{ fontSize: '14px' }}>班级</p>
+            <p className={styles.price}>
+              <span>￥{FY}</span>
+              <span>/学期</span>
+            </p>
+            <p className={styles.title} style={{ fontSize: '14px' }}>
+              班级
+            </p>
             <Radio.Group onChange={onBJChange} value={`${BJ}+${FY}`}>
-              {
-                KcDetail?.KHBJSJs?.map((value: { BJMC: string, id: string, FJS: string, FY: string, BMKSSJ: Date, BMJSSJ: Date, BJRS: number }, ind: number) => {
+              {KcDetail?.KHBJSJs?.map(
+                (
+                  value: {
+                    BJMC: string;
+                    id: string;
+                    FJS: string;
+                    FY: string;
+                    BMKSSJ: Date;
+                    BMJSSJ: Date;
+                    BJRS: number;
+                  },
+                  ind: number,
+                ) => {
                   // const text = `${value.BJMC}已有${() => getrs(value.id)}人报名，共${value.BJRS}个名额`;
                   const valueName = `${value.id}+${value.FY}`;
                   const start = value.BMKSSJ ? value.BMKSSJ : KcDetail.BMKSSJ;
                   const end = value.BMKSSJ ? value.BMJSSJ : KcDetail.BMJSSJ;
-                  const enAble = myDate >= new Date(moment(start).format('YYYY/MM/DD')) && myDate <= new Date(moment(end).format('YYYY/MM/DD'));
+                  const enAble =
+                    myDate >= new Date(moment(start).format('YYYY/MM/DD')) &&
+                    myDate <= new Date(moment(end).format('YYYY/MM/DD'));
                   return (
                     // <Tooltip placement="bottomLeft"  title={text} color='cyan' defaultVisible={true}>
-                    <Radio.Button value={valueName} style={{ marginRight: '14px' }}
+                    <Radio.Button
+                      value={valueName}
+                      style={{ marginRight: '14px' }}
                       disabled={!enAble}
                       onClick={() => butonclick(ind)}
                     >
                       {value.BJMC}
                     </Radio.Button>
                     // </Tooltip>
-                  )
-
-                })
-              }
+                  );
+                },
+              )}
             </Radio.Group>
             {/* <Radio
                     className={styles.agreement}
@@ -231,17 +287,31 @@ const CourseDetails: React.FC = () => {
                      </a>
                     </p>
                   </Radio> */}
-            <Button className={styles.submit}
-              disabled={fk || BJ === undefined}
-              onClick={submit} >
+            <Button className={styles.submit} disabled={fk || BJ === undefined} onClick={submit}>
               确定并付款
             </Button>
-            <Link style={{ visibility: 'hidden' }} ref={linkRef} to={{ pathname: '/parent/mine/orderDetails', state: { title: KcDetail?.KCMC, detail: classDetail, payOrder: orderInfo, user: currentUser, KKRQ: KcDetail?.KCRQ, JKRO: KcDetail?.KCRQ } }}>
-            </Link>
+            <Link
+              style={{ visibility: 'hidden' }}
+              ref={linkRef}
+              to={{
+                pathname: '/parent/mine/orderDetails',
+                state: {
+                  title: KcDetail?.KCMC,
+                  detail: classDetail,
+                  payOrder: orderInfo,
+                  user: currentUser,
+                  KKRQ: KcDetail?.KCRQ,
+                  JKRO: KcDetail?.KCRQ,
+                },
+              }}
+            ></Link>
           </div>
-        </div> : ''
-    }
-  </div>
+        </div>
+      ) : (
+        ''
+      )}
+    </div>
+  );
 };
 
 export default CourseDetails;
