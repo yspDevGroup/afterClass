8c40c620af05d4e2bbd11d6b38329a178c031db9
Liujin <1287784468@qq.com>
2021-09-07 20:34:02 +0800

    修改loading

--- config/config.ts
+++ config/config.ts
@@ -42,7 +42,7 @@ export default defineConfig({
     baseNavigator: true,
   },
   dynamicImport: {
-    loading: '@ant-design/pro-layout/es/PageLoading',
+    loading: '@/components/PageLoading',
   },
   targets: {
     ie: 11,
--- src/components/PageLoading.tsx
+++ src/components/PageLoading.tsx
@@ -1,10 +1,22 @@
-import type { FC } from "react";
+import type { FC } from 'react';
 import loadImg from '@/assets/loading.gif';
 
 const PageLoading: FC = () => {
   return (
-    <div><img style={{width:'50vw',maxWidth:'400px',margin:'0 auto',paddingTop:'15vh',display:'block'}} src={loadImg} /></div>
-  )
-}
+    <div style={{ width: '100vw', height: '100vh', background: 'rgba(255,255,255,0.2)' }}>
+      <img
+        src={loadImg}
+        alt="loading"
+        style={{
+          position: 'absolute',
+          top: '50%',
+          left: '50%',
+          transform: 'translate(-50%, -50%)',
+          width: '200px',
+        }}
+      />
+    </div>
+  );
+};
 
 export default PageLoading;
