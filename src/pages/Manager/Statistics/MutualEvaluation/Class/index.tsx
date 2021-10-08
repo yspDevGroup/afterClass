c6c3e0a8c91777278ca6b3b46108f1415304a086
gangxinhui <237301130@qq.com>
2021-10-08 09:49:24 +0800

    互评统计课程页面修改

--- src/pages/Manager/Statistics/MutualEvaluation/Class/index.tsx
+++ src/pages/Manager/Statistics/MutualEvaluation/Class/index.tsx
@@ -18,8 +18,8 @@ import { LeftOutlined } from '@ant-design/icons';
 const { Option } = Select;
 
 const school = (props: any) => {
-  // const {id} = props.location.state.data ;
-  // console.log(props.location.state.data);
+  const {id} = props.location.state.data ;
+  console.log(props.location.state.data);
 
   // 课程列表
   const [courseList, setcourseList] = useState<any>();
@@ -33,7 +33,7 @@ const school = (props: any) => {
   }
   useEffect(() => {
     (async () => {
-      const res = await getClassesEvaluation({ XNXQId: currentUser.XNXQId, })
+      const res = await getClassesEvaluation({ XNXQId: currentUser.XNXQId,KHKCSJId:id })
       console.log(res.data.rows);
 
       if (res.status === 'ok') {
