f9eaef3664b9b530576238c1c64f8c1dcec97933
zhupengliang <zpl_dongxie@sina.com>
2021-10-09 09:59:53 +0800

    修改password登录模式问题

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@
  * @description:
  * @author: zpl
  * @Date: 2021-06-07 16:02:16
- * @LastEditTime: 2021-10-08 19:07:12
+ * @LastEditTime: 2021-10-09 09:57:27
  * @LastEditors: zpl
  */
 import { useEffect } from 'react';
@@ -13,6 +13,15 @@ import loadImg from '@/assets/loading.gif';
 const Index = () => {
   const { initialState } = useModel('@@initialState');
 
+  const gotoLogin = (suiteID: string, isAdmin: string) => {
+    const loginPath = getLoginPath(suiteID, isAdmin);
+    if (loginPath.startsWith('http')) {
+      window.location.href = loginPath;
+    } else {
+      history.replace(loginPath);
+    }
+  };
+
   useEffect(() => {
     // 判断应用id和用户类型
     const query = getPageQuery();
@@ -22,47 +31,64 @@ const Index = () => {
     };
     params.suiteID = params.SuiteID || params.suiteID || '';
 
-    if (params.suiteID) {
-      // 应用id发生变化
-      const currentSuiteID = localStorage.getItem('suiteID');
-      if (params.suiteID !== currentSuiteID) {
-        const loginPath = getLoginPath(params.suiteID, params.isAdmin);
-        if (loginPath.startsWith('http')) {
-          window.location.href = loginPath;
-        } else {
-          history.replace(loginPath);
+    if (authType === 'wechat') {
+      if (params.suiteID) {
+        // 应用id发生变化
+        const currentSuiteID = localStorage.getItem('suiteID');
+        if (params.suiteID !== currentSuiteID) {
+          const loginPath = getLoginPath(params.suiteID, params.isAdmin);
+          if (loginPath.startsWith('http')) {
+            window.location.href = loginPath;
+          } else {
+            history.replace(loginPath);
+          }
+          return;
         }
-        return;
-      }
 
-      // 检查登录状态
+        // 检查登录状态
+        switch (initialState?.currentUser?.type) {
+          case '管理员':
+            history.replace('/homepage');
+            break;
+          case '老师':
+            if (params.isAdmin === '1') {
+              history.replace('/homepage');
+            } else {
+              history.replace('/teacher/home');
+            }
+            break;
+          case '家长':
+            history.replace('/parent/home');
+            break;
+          default:
+            gotoLogin(params.suiteID, params.isAdmin);
+            break;
+        }
+      } else {
+        history.replace('/403?message=未指定应用ID');
+      }
+    } else {
       switch (initialState?.currentUser?.type) {
         case '管理员':
           history.replace('/homepage');
           break;
         case '老师':
-          if (params.isAdmin === '1') {
-            history.replace('/homepage');
-          } else {
-            history.replace('/teacher/home');
-          }
+          history.replace('/teacher/home');
           break;
         case '家长':
           history.replace('/parent/home');
           break;
         default:
-          {
-            const loginPath = getLoginPath(params.suiteID, params.isAdmin);
-            if (loginPath.startsWith('http')) {
-              window.location.href = loginPath;
-            } else {
-              history.replace(loginPath);
-            }
-          }
+          gotoLogin('', '');
           break;
       }
-    } else {
-      history.replace('/403?message=未指定应用ID');
+      // if (initialState?.currentUser) {
+      //   // history.replace('/homepage');
+      //   // history.replace('/teacher/home');
+      //   history.replace('/parent/home');
+      // } else {
+      //   gotoLogin('', '')
+      // }
     }
 
     // if (initialState?.currentUser?.adminAuth?.includes('系统管理')) {
