97794770dc2597418e359ac4c760a167ee3e6a70
zhupengliang <zpl_dongxie@sina.com>
2021-10-12 14:56:42 +0800

    403页面显示调整

--- src/pages/403.tsx
+++ src/pages/403.tsx
@@ -2,11 +2,11 @@
  * @description: 鉴权失败界面
  * @author: zpl
  * @Date: 2021-07-14 17:11:16
- * @LastEditTime: 2021-10-12 12:57:59
+ * @LastEditTime: 2021-10-12 14:44:22
  * @LastEditors: zpl
  */
 import React from 'react';
-import { Result, Button } from 'antd';
+import { Result } from 'antd';
 import { getPageQuery } from '@/utils/utils';
 
 const NotFind = () => {
@@ -14,18 +14,30 @@ const NotFind = () => {
   return (
     <Result
       status="403"
-      title="抱歉，您无权访问此页面。"
-      subTitle={query.message || ''}
-      extra={
-        <Button
-          type="primary"
-          onClick={() => {
-            (window.top || window).location.href = '/';
-          }}
-        >
-          返回首页
-        </Button>
+      // title="抱歉，您无权访问此页面。"
+      subTitle={
+        <>
+          <span>抱歉，您无权访问此页面。</span>
+          {query.message ? (
+            <>
+              <br />
+              {query.message}
+            </>
+          ) : (
+            ''
+          )}
+        </>
       }
+      // extra={
+      //   <Button
+      //     type="primary"
+      //     onClick={() => {
+      //       (window.top || window).location.href = '/';
+      //     }}
+      //   >
+      //     返回首页
+      //   </Button>
+      // }
     />
   );
 };
