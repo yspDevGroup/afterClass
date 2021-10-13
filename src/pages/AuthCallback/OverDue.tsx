bae0e02ef75a3d0f440a2c0b455afc87ab91ed2b
zhupengliang <zpl_dongxie@sina.com>
2021-10-13 10:16:35 +0800

    修改token过期后微信端无法登录的问题

--- src/pages/AuthCallback/OverDue.tsx
+++ src/pages/AuthCallback/OverDue.tsx
@@ -1,10 +1,14 @@
 import Nodata from '@/components/Nodata';
+import { removeWechatInfo } from '@/utils/wx';
+import { removeOAuthToken } from '@/utils/utils';
 import overImg from '@/assets/loading.png';
 
 const OverDue = () => {
+  removeWechatInfo();
+  removeOAuthToken();
   return (
     <div>
-      <Nodata imgSrc={overImg} desc='认证失效，请关闭应用后重新进入' />
+      <Nodata imgSrc={overImg} desc="您还未登录或登录信息已过期，请关闭应用后重新进入" />
     </div>
   );
 };
