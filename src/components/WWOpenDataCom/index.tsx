3cdc07927a5faf4f47c57bda093820b85dc72cae
zhupengliang <zpl_dongxie@sina.com>
2021-10-28 11:20:38 +0800

    微信名称转义失败时显示用户ID

--- src/components/WWOpenDataCom/index.tsx
+++ src/components/WWOpenDataCom/index.tsx
@@ -2,10 +2,10 @@
  * @description: 企业微信通讯录展示组件
  * @author: zpl
  * @Date: 2021-09-17 14:41:57
- * @LastEditTime: 2021-10-14 16:55:36
+ * @LastEditTime: 2021-10-27 18:23:19
  * @LastEditors: zpl
  */
-import React, { useRef, useLayoutEffect } from 'react';
+import React, { useRef, useLayoutEffect, useState } from 'react';
 import type { CSSProperties } from '@umijs/renderer-react/node_modules/@types/react';
 
 type PropsType = {
@@ -14,11 +14,34 @@ type PropsType = {
   style?: CSSProperties;
 };
 export default function WWOpenDataCom({ type, openid, style = {} }: PropsType) {
+  const [showWechat, setShowWechat] = useState(true);
   const ref = useRef(null);
+
   useLayoutEffect(() => {
     if (openid && typeof WWOpenData !== 'undefined' && WWOpenData.bind) {
       WWOpenData.bind(ref.current);
+    } else {
+      setShowWechat(false);
     }
   }, [openid]);
-  return <ww-open-data style={{ color: '#333', ...style }} ref={ref} type={type} openid={openid} />;
+
+  if (showWechat) {
+    return (
+      <ww-open-data style={{ color: '#333', ...style }} ref={ref} type={type} openid={openid} />
+    );
+  }
+  return (
+    <span
+      style={{
+        color: '#333',
+        display: 'inherit',
+        maxWidth: '5em',
+        overflow: 'hidden',
+        textOverflow: 'ellipsis',
+        ...style,
+      }}
+    >
+      {openid}
+    </span>
+  );
 }
