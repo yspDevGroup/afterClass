e7885ce74d664b4d8bc62a2fab51bfbd0ede36f6
zhupengliang <zpl_dongxie@sina.com>
2021-10-29 18:24:58 +0800

    代码优化

--- config/config.9dy.ts
+++ config/config.9dy.ts
@@ -2,7 +2,7 @@
  * @description: 九朵云环境配置
  * @author: zpl
  * @Date: 2021-10-19 14:54:09
- * @LastEditTime: 2021-10-27 17:39:49
+ * @LastEditTime: 2021-10-29 17:55:28
  * @LastEditors: zpl
  */
 // https://umijs.org/config/
@@ -87,13 +87,4 @@ export default defineConfig({
       );
     }
   },
-  openAPI: [
-    {
-      requestLibPath: "import { request } from 'umi'",
-      schemaPath: 'http://api.test.xianyunshipei.com/documentation/json',
-      // schemaPath: 'http://192.168.0.113:3000/documentation/json',
-      // schemaPath: join(__dirname, 'oneapi.json'),
-      mock: false,
-    },
-  ],
 });
