83532b74441c0a038f559825169a74f47fbea65b
‘WuXing’ <1205118129@qq.com>
2021-10-30 12:33:32 +0800

    课堂风采2、4张图时宽高填充方式调整

--- src/pages/Parent/Homepage/Study/ClassroomStyle/index.tsx
+++ src/pages/Parent/Homepage/Study/ClassroomStyle/index.tsx
@@ -72,7 +72,7 @@ const ClassroomStyle = () => {
                   <p>{item.content}</p>
                   <div className={styles.imgContainer}>
                   <Image.PreviewGroup>
-                    <Row gutter={[6, 6]} style={{width: '100%',height: item.imgs.length === (2 || 4) ? '100px': (item.imgs.length === 1? 'auto' : '280px')}}>
+                    <Row gutter={[6, 6]} style={{width: '100%'}}>
                     {
                       item.imgs.map((url: string)=> {
                         return <Col span={(item.imgs.length === 2 || item.imgs.length === 4) ? 12 : (item.imgs.length === 1 ? 24 : 8)} className={(item.imgs.length === 2 || item.imgs.length === 4) ? styles.pairImg: (item.imgs.length === 1 ? styles.oneImg : styles.nineImg)}>
