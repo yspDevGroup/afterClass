8fbf29f1e6611efcc7e9a05ae6dc96a0609d2913
WeiShiLong <979803984@qq.com>
2021-11-26 15:55:52 +0800

    接口更新

--- src/services/after-class/khxspj.ts
+++ src/services/after-class/khxspj.ts
@@ -11,7 +11,7 @@ export async function getKHXSPJ(
   },
   options?: { [key: string]: any },
 ) {
-  const { id: param0 } = params;
+  const { id: param0, ...queryParams } = params;
   return request<{
     status?: 'ok' | 'error';
     data: {
@@ -48,7 +48,7 @@ export async function getKHXSPJ(
     message?: string;
   }>(`/khxspj/${param0}`, {
     method: 'GET',
-    params: { ...params },
+    params: { ...queryParams },
     ...(options || {}),
   });
 }
@@ -62,10 +62,10 @@ export async function deleteKHXSPJ(
   },
   options?: { [key: string]: any },
 ) {
-  const { id: param0 } = params;
+  const { id: param0, ...queryParams } = params;
   return request<{ status?: 'ok' | 'error'; message?: string }>(`/khxspj/${param0}`, {
     method: 'DELETE',
-    params: { ...params },
+    params: { ...queryParams },
     ...(options || {}),
   });
 }
@@ -158,13 +158,13 @@ export async function updateKHXSPJ(
   body: API.UpdateKHXSPJ,
   options?: { [key: string]: any },
 ) {
-  const { id: param0 } = params;
+  const { id: param0, ...queryParams } = params;
   return request<{ status?: 'ok' | 'error'; message?: string }>(`/khxspj/update/${param0}`, {
     method: 'PUT',
     headers: {
       'Content-Type': 'application/json',
     },
-    params: { ...params },
+    params: { ...queryParams },
     data: body,
     ...(options || {}),
   });
--- src/services/after-class/khxxzzfw.ts
+++ src/services/after-class/khxxzzfw.ts
@@ -77,7 +77,7 @@ export async function KHXXZZFW(
   },
   options?: { [key: string]: any },
 ) {
-  const { id: param0 } = params;
+  const { id: param0, ...queryParams } = params;
   return request<{
     status?: 'ok' | 'error';
     data: {
@@ -98,7 +98,7 @@ export async function KHXXZZFW(
     message?: string;
   }>(`/khxxzzfw/${param0}`, {
     method: 'GET',
-    params: { ...params },
+    params: { ...queryParams },
     ...(options || {}),
   });
 }
@@ -112,10 +112,10 @@ export async function deleteKHXXZZFW(
   },
   options?: { [key: string]: any },
 ) {
-  const { id: param0 } = params;
+  const { id: param0, ...queryParams } = params;
   return request<{ status?: 'ok' | 'error'; message?: string }>(`/khxxzzfw/${param0}`, {
     method: 'DELETE',
-    params: { ...params },
+    params: { ...queryParams },
     ...(options || {}),
   });
 }
@@ -130,13 +130,13 @@ export async function updateKHXXZZFW(
   body: API.UpdateKHXXZZFW,
   options?: { [key: string]: any },
 ) {
-  const { id: param0 } = params;
+  const { id: param0, ...queryParams } = params;
   return request<{ status?: 'ok' | 'error'; message?: string }>(`/khxxzzfw/update/${param0}`, {
     method: 'PUT',
     headers: {
       'Content-Type': 'application/json',
     },
-    params: { ...params },
+    params: { ...queryParams },
     data: body,
     ...(options || {}),
   });
