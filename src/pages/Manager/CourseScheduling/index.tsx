2d5048f195e2b5940915c63e6d7b3c3f32434b41
wuzhan <1076795708@qq.com>
2021-12-30 16:01:07 +0800

    修改 课程班管理跳转排课

--- src/pages/Manager/CourseScheduling/index.tsx
+++ src/pages/Manager/CourseScheduling/index.tsx
@@ -12,26 +12,32 @@ const { TabPane } = Tabs;
 
 const Index = (props: any) => {
   const [key, setKey] = useState<string>('1');
+  const [falg,setFalg]=useState<boolean>(true);
 
   useEffect(() => {
     const bjId = getQueryString('courseId');
     if (bjId !== null) {
+      setFalg(false);
       setKey('2')
     }
   }, [])
 
   return (
     <PageContainer>
-      <Tabs activeKey={key} onChange={(value: any) => {
-        setKey(value)
-      }}>
-        <TabPane tab="行政班排课" key="1">
-          { key==='1' &&<ClassScheduling />}
-        </TabPane>
-        <TabPane tab="课程班排课" key="2">
-        { key==='2' &&<CourseScheduling />}
-        </TabPane>
-      </Tabs>
+      {
+        falg? <Tabs activeKey={key} onChange={(value: any) => {
+          setKey(value)
+        }}>
+          {<TabPane tab="行政班排课" key="1">
+            { key==='1' &&<ClassScheduling />}
+          </TabPane>
+          }
+          <TabPane tab="课程班排课" key="2">
+          { key==='2' &&<CourseScheduling />}
+          </TabPane>
+        </Tabs>: <CourseScheduling />
+      }
+     
     </PageContainer>
   );
 };
