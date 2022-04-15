da3bb21b98fc36484cf8d2aa12b003f8aa57d26c
WeiShiLong <979803984@qq.com>
2022-04-15 09:29:15 +0800

    管理端移动端收款统计调整

--- src/pages/Information/HomePage/Home/components/Overview.tsx
+++ src/pages/Information/HomePage/Home/components/Overview.tsx
@@ -17,48 +17,62 @@ const Overview = () => {
       const result = await queryXNXQList(currentUser?.xxId);
       const res = await homePage({
         XXJBSJId: currentUser?.xxId,
-        XNXQId: result.current?.id
+        XNXQId: result.current?.id,
       });
       if (res.status === 'ok') {
         setHomeData({
           ...res.data,
           xs_counts: Number(res?.data?.xs_count + res?.data?.khfwxs_count),
-          tk_amounts: Number(res?.data?.tk_amount + res?.data?.khtk_amount)
+          tk_amounts: Number(res?.data?.tk_amount + res?.data?.khtk_amount),
+          bjdd_amount: Number(
+            res?.data?.bjdd_amount + res?.data?.khfw_amount + res?.data?.zzfw_amount,
+          ),
         });
-      };
+      }
     }
     fetchData();
-  }, [])
-
+  }, []);
 
   const ItemCard = (props: any) => {
     const { title, count, bgImg, zzfw } = props;
     return (
-      <Card className={styles.card} bordered={false} bodyStyle={{ paddingTop: 8.8, paddingLeft: 8.8, minHeight: '101.7px' }}>
-        <p style={{ whiteSpace: 'nowrap', overflow: 'hidden', textOverflow: 'ellipsis' }}>{title}</p>
-        <p>{title.indexOf("元") !== -1 ? (title.indexOf("收费") !== -1 ? ((count || 0) + (zzfw || 0)).toFixed(2) : parseFloat((count || 0)).toFixed(2)) : (count || 0)}</p>
+      <Card
+        className={styles.card}
+        bordered={false}
+        bodyStyle={{ paddingTop: 8.8, paddingLeft: 8.8, minHeight: '101.7px' }}
+      >
+        <p style={{ whiteSpace: 'nowrap', overflow: 'hidden', textOverflow: 'ellipsis' }}>
+          {title}
+        </p>
+        <p>
+          {title.indexOf('元') !== -1
+            ? title.indexOf('收费') !== -1
+              ? ((count || 0) + (zzfw || 0)).toFixed(2)
+              : parseFloat(count || 0).toFixed(2)
+            : count || 0}
+        </p>
         <img className={styles.bgImg} src={bgImg} alt="" />
       </Card>
-    )
-  }
+    );
+  };
 
   return (
     <div className={styles.overview}>
-      <Tabs
-        centered={false}
-      >
+      <Tabs centered={false}>
         <TabPane tab="本学期概述" key="semester">
           <Row gutter={[8, 8]}>
             {topNum.map((item, index) => {
-              return <Col className="gutter-row" span={8}>
-                <ItemCard
-                  title={item.title}
-                  count={homeData ? homeData[item.type] : 0}
-                  bgImg={item.bgImg}
-                  key={index}
-                  zzfw={(homeData?.zzfw_amount ? homeData?.zzfw_amount : 0)}
-                />
-              </Col>
+              return (
+                <Col className="gutter-row" span={8}>
+                  <ItemCard
+                    title={item.title}
+                    count={homeData ? homeData[item.type] : 0}
+                    bgImg={item.bgImg}
+                    key={index}
+                    zzfw={homeData?.zzfw_amount ? homeData?.zzfw_amount : 0}
+                  />
+                </Col>
+              );
             })}
           </Row>
         </TabPane>
@@ -68,3 +82,4 @@ const Overview = () => {
 };
 
 export default Overview;
+
--- src/pages/Information/HomePage/Statistical/c-pages/Toll.tsx
+++ src/pages/Information/HomePage/Statistical/c-pages/Toll.tsx
@@ -15,47 +15,41 @@ import ModuleTitle from '../components/ModuleTitle';
 import NumberCollect from '../components/NumberCollect';
 import { queryXNXQList } from '@/services/local-services/xnxq';
 
-
 const Toll = () => {
   const { initialState } = useModel('@@initialState');
   const { currentUser } = initialState || {};
-  const [startTime, setStartTime] = useState<any>(moment().subtract(30, "days").format("YYYY-MM-DD"));
-  const [endTime, setEndTime] = useState<any>(moment().subtract(-1, "days").format("YYYY-MM-DD"));
+  const [startTime, setStartTime] = useState<any>(
+    moment().subtract(30, 'days').format('YYYY-MM-DD'),
+  );
+  const [endTime, setEndTime] = useState<any>(moment().subtract(-1, 'days').format('YYYY-MM-DD'));
   const [currentData, setCurrentData] = useState<any>([
     {
       title: '收款金额（元）',
-      num: '--'
+      num: '--',
     },
     {
       title: '退款金额（元）',
-      num: '--'
-    }]);
+      num: '--',
+    },
+  ]);
   const [intervalData, setIntervalData] = useState<any>([
     {
       num: '--',
-      title: '收款金额（元）'
-    }, {
+      title: '收款金额（元）',
+    },
+    {
       num: '--',
-      title: '退款金额（元）'
-    }
+      title: '退款金额（元）',
+    },
   ]);
 
-const handleStartTime = (date: any) => {
-  setStartTime(moment(date).format('YYYY-MM-DD'));
-}
-const handleEndTime = async (date: any) => {
-  setEndTime(moment(date).format('YYYY-MM-DD'));
-}
-
-useEffect(()=>{
-    const res = getTerm();
-    getData(res);
-},[endTime])
-
-
-
-  const getData = async (res: any) => {
-
+  const handleStartTime = (date: any) => {
+    setStartTime(moment(date).format('YYYY-MM-DD'));
+  };
+  const handleEndTime = async (date: any) => {
+    setEndTime(moment(date).format('YYYY-MM-DD'));
+  };
+  const getData = async () => {
     const defaultData: any = {
       serviceNum: [],
       courseNum: [],
@@ -64,19 +58,24 @@ useEffect(()=>{
     const xnxqResult = await queryXNXQList(currentUser?.xxId);
     const tollRes = await homePage({
       XXJBSJId: currentUser?.xxId,
-      XNXQId: xnxqResult.current?.id
+      XNXQId: xnxqResult.current?.id,
     });
-    if(tollRes.status === 'ok'){
+    if (tollRes.status === 'ok') {
       defaultData.serviceNum = [
-      {
-        title: '收款金额（元）',
-        num:((tollRes.data?.bjdd_amount || 0) + (tollRes.data?.zzfw_amount || 0)).toFixed(2)
-      },
-      {
-        title: '退款金额（元）',
-        num: tollRes.data?.tk_amount || 0
-      }];
-      tollRes.data.kcbm.forEach((item: any)=>{
+        {
+          title: '收款金额（元）',
+          num: (
+            (tollRes.data?.bjdd_amount || 0) +
+            (tollRes.data?.zzfw_amount || 0) +
+            (tollRes.data?.khfw_amount || 0)
+          ).toFixed(2),
+        },
+        {
+          title: '退款金额（元）',
+          num: (tollRes.data?.tk_amount || 0) + (tollRes.data?.khtk_amount || 0),
+        },
+      ];
+      tollRes.data.kcbm.forEach((item: any) => {
         defaultData.courseNum.push({
           type: '收款',
           label: item.KCMC,
@@ -86,9 +85,9 @@ useEffect(()=>{
           type: '退款',
           label: item.KCMC,
           value: parseFloat(item.tk_amount) || 0,
-        })
+        });
         tollBarConfig.data = defaultData.courseNum;
-      })
+      });
     }
 
     const fundRes = await getRefund({
@@ -96,83 +95,85 @@ useEffect(()=>{
       startDate: startTime,
       endDate: endTime,
     });
-    console.log('时间区间查询收退款: ', fundRes);
-    if(fundRes.status === 'ok'){
-      console.log('fundRes: ', fundRes);
+    if (fundRes.status === 'ok') {
       setIntervalData([
         {
           num: ((fundRes.data.sk_amount || 0) + (fundRes.data?.zzfw_amount || 0)).toFixed(2),
-          title: '收款金额（元）'
-        }, {
+          title: '收款金额（元）',
+        },
+        {
           num: fundRes.data.tk_amount,
-          title: '退款金额（元）'
-        }
+          title: '退款金额（元）',
+        },
       ]);
     }
-
-
-
-    // const result = await getScreenInfo({
-    //   ...res,
-    //   XZQHM: currentUser?.XZQHM
-    // });
-    // if (result.status === 'ok') {
-    //   const { data } = result;
-    //   if (data) {
-    //     }) : [];
-    //   }
-    // }
     setCurrentData(defaultData.serviceNum);
   };
+  useEffect(() => {
+    getData();
+  }, [endTime]);
 
   useEffect(() => {
-    const res = getTerm();
-    getData(res);
+    getData();
   }, []);
 
   return (
     <div className={styles.toll}>
       <div className={styles.container} style={{ height: '136px' }}>
-        <ModuleTitle data='本学期收费总计' />
+        <ModuleTitle data="本学期收费总计" />
         <NumberCollect data={currentData} col={currentData?.length} />
       </div>
       <div className={styles.container} style={{ height: '700px' }}>
-        <ModuleTitle data='各课程收退款情况' />
+        <ModuleTitle data="各课程收退款情况" />
         <div className={styles.chartsContainer}>
-          {
-            (tollBarConfig.data && tollBarConfig.data?.length!==0) ? <Bar {...tollBarConfig} /> : <Empty
-            image={noData}
-            imageStyle={{
-              minHeight: 200
-            }}
-            style={{minHeight: 355}}
-            description={'暂无收退款信息'} />
-          }
+          {tollBarConfig.data && tollBarConfig.data?.length !== 0 ? (
+            <Bar {...tollBarConfig} />
+          ) : (
+            <Empty
+              image={noData}
+              imageStyle={{
+                minHeight: 200,
+              }}
+              style={{ minHeight: 355 }}
+              description={'暂无收退款信息'}
+            />
+          )}
         </div>
       </div>
       <div className={styles.container} style={{ height: '192px' }}>
-        <ModuleTitle data='收费统计查询' showRight={false} />
-        <Space direction="vertical" style={{marginTop: '20px'}} size={12}>
+        <ModuleTitle data="收费统计查询" showRight={false} />
+        <Space direction="vertical" style={{ marginTop: '20px' }} size={12}>
           <Row>
             <ConfigProvider locale={locale}>
               <Col span={11}>
-                <DatePicker placeholder='请选择开始日期' defaultValue={moment(moment().subtract(30, "days"), 'YYYY-MM-DD')} onChange={handleStartTime} format="YYYY-MM-DD"/>
+                <DatePicker
+                  placeholder="请选择开始日期"
+                  defaultValue={moment(moment().subtract(30, 'days'), 'YYYY-MM-DD')}
+                  onChange={handleStartTime}
+                  format="YYYY-MM-DD"
+                />
               </Col>
               <Col span={2}>-</Col>
               <Col span={11}>
-                <DatePicker placeholder='请选择结束日期' defaultValue={moment(moment().subtract(-1, "days"), 'YYYY-MM-DD')} onChange={handleEndTime} format="YYYY-MM-DD"/>
+                <DatePicker
+                  placeholder="请选择结束日期"
+                  defaultValue={moment(moment().subtract(-1, 'days'), 'YYYY-MM-DD')}
+                  onChange={handleEndTime}
+                  format="YYYY-MM-DD"
+                />
               </Col>
             </ConfigProvider>
           </Row>
         </Space>
         <Row>
           <Col span={24}>
-          <NumberCollect data={intervalData} col={intervalData?.length} />
+            <NumberCollect data={intervalData} col={intervalData?.length} />
           </Col>
         </Row>
       </div>
-    </div>)
-}
+    </div>
+  );
+};
 
 export default Toll;
 
